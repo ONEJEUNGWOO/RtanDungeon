6f092a24a43ea3668ccced204d55abd7f5aedb1d
원정우 <rjstn155@naver.com>
2025-08-10 21:35:47 +0900

    [Feat] 도전기능 움직이는 플렛폼 움직임만 구현 / 올라타면 물리작용으로 움직임이 멈춤 수정 필요

--- Assets/04_Scripts/JumpPoint.cs
+++ Assets/04_Scripts/JumpPoint.cs
@@ -5,12 +5,47 @@ using UnityEngine;
 public class JumpPoint : MonoBehaviour
 {
     public int JumpPower;
+    public int moveSpeed;
+    public float moveChangeTime;
+    public bool isMove;
+    private bool isCoroutine = true;
+    private Rigidbody _rigidbody;
 
-    private void OnTriggerEnter(Collider other)
+    private void Awake()
     {
-        if (!other.CompareTag("Player"))
-            return;
+        _rigidbody = GetComponent<Rigidbody>();
+    }
 
-        other.GetComponent<Rigidbody>().AddForce(Vector3.up * JumpPower, ForceMode.Impulse);
+    private void FixedUpdate()
+    {
+        if (isMove && isCoroutine)
+        {
+            Debug.Log("Ω√¿€");
+            StartCoroutine(MovingHoraizontal());
+        }
+    }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (!other.CompareTag("Player"))
+    //        return;
+
+    //    other.GetComponent<Rigidbody>().AddForce(Vector3.up * JumpPower, ForceMode.Impulse);
+    //}
+
+    private IEnumerator MovingHoraizontal()
+    {
+        isCoroutine = false;
+        _rigidbody.velocity = transform.right * moveSpeed;  
+        yield return new WaitForSeconds(moveChangeTime);
+        _rigidbody.velocity = -transform.right * moveSpeed;
+        yield return new WaitForSeconds(moveChangeTime);
+
+        _rigidbody.velocity = -transform.right * moveSpeed; //ø’∫π
+        yield return new WaitForSeconds(moveChangeTime);
+        _rigidbody.velocity = transform.right * moveSpeed;
+        yield return new WaitForSeconds(moveChangeTime);
+        isCoroutine = true;
+
     }
 }
